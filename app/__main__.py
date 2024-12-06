169d297a9b1e7281ce6b7847bf274b3ced71db5c
roo7690 <roosevelt@roo7690.me>
2024-12-06 08:04:00 +0000

    gguf model ...

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,37 @@
-from dotenv import load_dotenv
 import os
-from transformers import AutoTokenizer,TFAutoModel
+from argparse import ArgumentParser, ArgumentTypeError
+import transformers
+def init(model:str,auto_model:str='AutoModelForCausalLM'):
+  os.tokenizer=transformers.AutoTokenizer.from_pretrained(model)
+  os.model=getattr(transformers,auto_model).from_pretrained(model)
 
-load_dotenv('.env.local')
-tokeniser=AutoTokenizer.from_pretrained(os.getenv('MODEL_PATH'))
-model=TFAutoModel.from_pretrained(os.getenv('MODEL_PATH'))
+def chat():
+  inputs=input()
+  fn=handler.get(inputs,_chat(inputs))
+  fn()
+  chat()
+
+handler={
+  ":q":exit
+}
+
+def _chat(_input:str):
+  def __chat():
+    inputs=os.tokenizer(_input,return_tensors="pt")
+    output=os.model(**inputs)
+    print(os.tokenizer.decode(output.logits,skip_special_tokens=True))
+  return __chat
+
+if __name__=='__main__':
+  parser=ArgumentParser()
+  parser.add_argument('-M',type=str,help='Sélection du modèle à utiliser')
+  parser.add_argument('-l',type=str,help='La classe utilise pour charger le modele')
+  args=parser.parse_args()
+  if args.M is None:
+    raise ArgumentTypeError('Aucun modèle choisi')
+  if args.l:
+    init(args.M,args.l)
+  else:
+    init(args.M)
+  print(f'Lancement de chat avec {os.model.config.model_type}')
+  chat()
